768dcd7e84ec10ee3219e74a958ccd0192553b99
sfrancop <s.francop@uniandes.edu.co>
2021-11-06 17:00:55 -0500

    links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import commentsJson from "./comments.json"
-import CommentList from "./components/Comments"
 import CommentForm from './components/CommentForm';
-import Navbar from "./components/Navbar"
 import QuienesSomos from './components/QuienesSomos';
 import Equipo from "./components/Equipo"
 import ComentariosDestacados from "./components/ComentariosDestacados"
 import ContactComponent from "./components/ContactComponent"
-import HistorialReservas from "./components/HistorialReservas"
 
 import "./components//styles/main.css"
 import "./components//styles/bootstrap.min.css"
@@ -61,16 +58,16 @@ class App extends Component{
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <Link to="/" class="nav-link text-light" aria-current="page" href="index.html">Inicio</Link>
+                    <a href="https://sfrancop.github.io/restprimavera/index.html" class="nav-link text-light" aria-current="page">Inicio</a>
                   </li>
                   <li class="nav-item">
                     <Link to="/nosotros" class="nav-link text-light" aria-current="page" href="Nosotros.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nosotros</Link>
                   </li>
                   <li class="nav-item">
-                    <Link to="/menu" class="nav-link text-light" href="menú.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menú</Link>
+                  <a href="https://sfrancop.github.io/restprimavera/menú.html" class="nav-link text-light" aria-current="page">Menú</a>
                   </li>
                   <li class="nav-item">
-                    <Link to="/servicios" class="nav-link text-light" href="Servicios.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Servicios</Link>
+                  <a href="https://sfrancop.github.io/restprimavera/Servicios.html" class="nav-link text-light" aria-current="page">Servicios</a>
                   </li>
                   <li class="nav-item">
                     <Link to="/contactanos" class="nav-link text-light" href="contactanos.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contáctanos</Link>
@@ -104,7 +101,7 @@ class App extends Component{
             <CommentForm addComment={this.addComment}/>
             {this.state.comments.map(e => <div class="parrafo"><h1>{e.name}</h1><p>{e.comment}</p></div>)}
           </Route>
-          <Route path="/">
+          <Route path="https://sfrancop.github.io/restprimavera/index.html">
             <div>
               Inicio
             </div>
